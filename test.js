de656b522092c5b6355e26dadd22e78a75295cb3
Srilakshmi Bontu <Srilakshmi.Bontu@exostarus.onmicrosoft.com>
2022-09-26 08:34:39 -0400

    test

---
+++ test.js
@@ -0,0 +1,29 @@
+const OTP = "delete";
+
+switch (OTP ){
+    case "Register" :
+       console.log("registerOTP")
+       break;
+       case "elevate":
+        console.log("elevateOTP")
+        break;
+        case "manage":
+            console.log("manageOTP")
+        break;
+        case "delete":
+            console.log("deleteOTP")
+            break;
+            default : console.log("nothing matched")
+}
+
+let apps= ["ezmQualification","EZMmsd.com","EZMmobile","EZMLargedata"]
+// sectionLinks="table>tbody>tr>td>span> a"
+// let apps= "sectionlinks"
+for(let i=0; i<apps.length; i++){
+    if(apps[i]==="EZMmobile"){
+       var appname=apps[i] 
+        console.log(apps[i])
+        break;
+    }
+ }
+
