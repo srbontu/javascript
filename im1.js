5e9d30774bf4c4d6542c7f6a09b6784b20d4ea01
Srilakshmi Bontu <Srilakshmi.Bontu@exostarus.onmicrosoft.com>
2022-10-04 09:03:31 -0400

    new script

--- im1.js
+++ im1.js
@@ -1,19 +1,25 @@
-import { products1 } from "./e1";
+import { products1 } from "./e1.js";
 
- const productsNames= ("SAM");
+ const productsNames= "Secure Access Manager";
  if(productsNames===products1.MAG){
     console.log(products1)
-//  }else if(productsNames===products1.BAID){
-//     console.log(products1.BAID)
-//  }else if(productsNames===products1.SOTP){
-//     console.log(products1.SOTP)
-//  }else if(productsNames===products1.SCP){
-//     console.log(products1.SCP)
-//  }else if(productsNames===products1.SAM){
-//     console.log("This is the correct product"+" "+products1.SAM)
+ }else if(productsNames===products1.BAID){
+    console.log(products1.BAID)
+ }else if(productsNames===products1.SOTP){
+    console.log(products1.SOTP)
+ }else if(productsNames===products1.SCP){
+    console.log(products1.SCP)
+ }else if(productsNames===products1.SAM){
+    console.log("This is the correct product")
  }else{
     console.log("default")
  }
 
-    
+    const fun1=()=>{
+
+    }
+
+   function fu2(){
+
+    }
  
